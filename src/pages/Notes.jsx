e12a78b3230aea722c0ce0a9613837a3d34c7963
dzikra24ti <dzikra24ti@mahasiswa.pcr.ac.id>
2026-06-17 09:34:31 +0700

    P13

--- src/pages/Notes.jsx
+++ src/pages/Notes.jsx
@@ -102,7 +102,7 @@ export default function Notes() {
   return (
     <div className="max-w-2xl mx-auto p-6">
       {/* Menggunakan komponen PageHeader yang di-import */}
-      <PageHeader title="Notes App" />
+  <PageHeader title="Notes" currentBreadcrumb="Notes List" />
 
       {/* Form Card */}
       <div className="bg-white rounded-2xl shadow-lg p-6 mt-6">
